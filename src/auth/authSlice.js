10025df260ec40373dab444eb23d083aeebad114
Harry <harsagra3478@gmail.com>
2024-03-02 10:57:07 +0530

    login signup

--- src/auth/authSlice.js
+++ src/auth/authSlice.js
@@ -1,11 +1,14 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-
+import { redirect, useNavigate } from "react-router-dom";
+import { browserHistory } from "react-router";
 // const loginOtpAsync = createAsyncThunk(
 //     "login",
 //     async (email) => {
 //         await fetch(process.env.REACT_APP_BACKEND_URL + "/login/otp").then(res=>())
 //     }
 // )
+
+
 export const signupWithOtpAsync = createAsyncThunk(
     "signup",
     async (data) => {
@@ -16,7 +19,7 @@ export const signupWithOtpAsync = createAsyncThunk(
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify(data)
-            
+
             })
             if (!res.ok) {
                 const r = await res.json();
@@ -24,7 +27,7 @@ export const signupWithOtpAsync = createAsyncThunk(
             }
             return await res.json();
 
-        } catch(e) { 
+        } catch (e) {
             throw new Error(e.message);
         }
     }
@@ -35,7 +38,23 @@ const authSlice = createSlice({
     initialState: {
         name: "harshit",
         user: "null",
+        registered: false,
         state: "idle",
+    },
+    extraReducers: builder => {
+        builder.addCase(signupWithOtpAsync.pending, (state, action) => {
+            state.state = "loading";
+        })
+        builder.addCase(signupWithOtpAsync.fulfilled, (state, action) => {
+
+            state.state = "idle";
+            state.user = action.payload.user;
+            state.registered = true;
+        })
+        builder.addCase(signupWithOtpAsync.rejected, (state, action) => {
+            state.state = "idle";
+            state.user = "null";
+        })
     }
 })
 
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,6 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "./Input";
+import emailIcon from "../images/email.png";
+import { useNavigate } from "react-router-dom";
 
-
-export default function(){
-    
+export default function Login(){
+    const [email, setEmail] = useState("");
+    const navigate = useNavigate();
+    async function handleClick(){
+        await fetch(process.env.REACT_APP_BACKEND_URL + "login/otp", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({email})
+        }).then(res => res.json()).then(data => {
+            console.log(data);
+            if(data.error){
+                alert(data.error);
+            }else{
+                alert("OTP sent to your email");
+                navigate("/register/verify", {state: {email , isSignup : false}});
+            }
+        }
+        )
+    }
+    return(
+        <>
+        <div className="reg-header">
+          <div className="reg-text">login</div>
+          <div className="reg-underline"></div> 
+         </div>
+        <div className="flex flex-col justify-center items-center gap-7 m-10">
+        <Input placeholder="email" type="text" icon={emailIcon} value={email} setValue={setEmail} />
+        <button className="bg-black w-1/5 m-3 p-4 rounded-md text-white" onClick={handleClick}>Login</button>
+        </div>
+        </>
+    )
 }
