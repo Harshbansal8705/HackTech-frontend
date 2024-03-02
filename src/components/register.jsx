ec764c206bd7aa760c9e9faff7af2d907c71cba1
vishnu-1817 <139904717+vishnu-1817@users.noreply.github.com>
2024-03-02 14:19:16 +0530

    Report Pdf

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -50,7 +50,7 @@ export default function Register() {
                 method: "POST",
                 headers: {
                      "Content-Type": "application/json"
-                },
+                },  
                 body: JSON.stringify({email})
               }).then(res => res.json()).then(data => {
                 console.log(data);
