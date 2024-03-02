0e58d443663e22bbebe10bcb361c7a6f5b347cae
ayerhssb <shreyabhartissb1@gmail.com>
2024-03-02 07:10:45 +0530

    landing page basic design

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -1,13 +1,32 @@
-import react from "react";
+import React from "react";
+import "./styles/landingpage.css";
+import { Link } from "react-router-dom";
 
+const LandingPage = () => {
+  return (
+    <>
+      <div className="container-landingpage">
+        <div className="left-landingpage">
+          <pre>
+            <h1 className="heading">
+              {"Welcome Buddy!\nLet’s optimize your "}<span style={{color:"#296dd0"}}>{"Breadth selection"}</span>
+            </h1>
+          </pre>
 
-const LandingPage = ()=>{
+          <p className="text-landingpage">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore .
+          </p>
 
-    return(
-       <>
-       <h1>landingpage</h1>
-       </> 
-    )
-}
+          <Link to={"/register2"}><button className="get-started">Get Started</button></Link>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default LandingPage;
